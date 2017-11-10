daa1548d4a1512d54dfdbb69f5f3dd39ad572614
Astrocat2374 <sugiyama.evan@gmail.com>
2017-11-09 21:30:34 -1000

    Push final

--- conversion.js
+++ conversion.js
@@ -3,6 +3,7 @@ var c = [];
 for (farenheit = 20; farenheit <= 120; farenheit += 5){
 
 	celcius = (farenheit - 32) * (5 / 9);
+	celcius = Math.round(celcius * 100) / 100;
 	c[farenheit] = celcius;
 
 }

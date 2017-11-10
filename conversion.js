0e938ab7da99d0cf512e9ff09727ce258757e031
Astrocat2374 <sugiyama.evan@gmail.com>
2017-11-09 14:44:49 -1000

    Added an array to store values

--- conversion.js
+++ conversion.js
@@ -1,24 +1,8 @@
+var c = [];	
 
+for (farenheit = 20; farenheit < 120; farenheit += 5){
 
+	celcius = (farenheit - 32) * (5 / 9);
+	c[farenheit] = celcius;
 
-
-
-
-
-
-
-
-
-var f = [];
-var c = [];
-
-for (farenheit = 20; farenheit < 120; farenheit+=5){
-	
-	function toCelcius(farenheit){
-		return (farenheit-32) * (5/9);
-		f[farenheit] = farenheit;
-		c[farenheit] = toCelcius;
-	}
-	return f;
-	return c;
 }

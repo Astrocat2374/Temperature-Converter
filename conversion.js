d34716a1cb4319759cf8c9bf2d382488d7103367
Astrocat2374 <sugiyama.evan@gmail.com>
2017-11-09 14:54:25 -1000

    Blyat 4

--- conversion.js
+++ conversion.js
@@ -1,6 +1,6 @@
 var c = [];	
 
-for (farenheit = 20; farenheit < 120; farenheit += 5){
+for (farenheit = 20; farenheit <= 120; farenheit += 5){
 
 	celcius = (farenheit - 32) * (5 / 9);
 	c[farenheit] = celcius;

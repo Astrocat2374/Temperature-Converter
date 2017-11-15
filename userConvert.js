19e93a05e19e61e6144f67acc9474961d224122f
Astrocat2374 <sugiyama.evan@gmail.com>
2017-11-14 15:01:58 -1000

    Working out user input

--- userConvert.js
+++ userConvert.js
@@ -1,13 +1,3 @@
-var c = [];	
-
-for (farenheit = 20; farenheit <= 120; farenheit += 5){
-
-	celcius = (farenheit - 32) * (5 / 9);
-	celcius = Math.round(celcius * 100) / 100;
-	c[farenheit] = celcius;
-
-}
-
 function userConvert() {
 	var temp = document.getElementByID("temp");
 	var unit = document.getElementByID("convert");
